e37e8aa84df94124df4645efdc5c1bc805fe0f12
Benjamin Morris <bmorris458@gmail.com>
2017-12-07 17:30:54 -0500

    Add cli input read and echo repl as main loop

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM rust:1.22.1
 COPY ./driver /
 RUN cargo build
 
-CMD ["./target/debug/driver"]
+CMD ["./target/debug/driver", "hello", "world"]
 
 # docker build -t driver:<tag> .
 # docker run -t driver:<tag>
--- driver/src/main.rs
+++ driver/src/main.rs
@@ -1,3 +1,36 @@
+//extern crate serial;
+
+use std::env;
+use std::io;
+//use std::time::Duration;
+
+//use std::io::prelude::*;
+//use serial::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+	println!("Initialization arguments:");
+	for arg in env::args_os().skip(1) {
+		arg.to_str()
+			.map(|s| println!("- {}", s));
+	}
+
+	loop {
+		print!("> ");
+
+		let mut input = String::new();
+
+		io::stdin().read_line(&mut input)
+			.expect("Failed to read line");
+		send(&input).unwrap();
+
+		match input.trim() {
+			"exit" => break,
+			_ => continue,
+		}
+	}
+}
+
+fn send(msg: &str) -> io::Result<()> {
+	println!("{}", msg);
+	Ok(())
 }
